5692b4d3189607a34ea665595df942d41b4b9386
Yamami <yamami.fujita@gmail.com>
2025-01-13 00:50:13 +0700

    Organize API and Path constants for improved readability and structure

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,7 +1,9 @@
 export enum Api {
+  APPLICATION = '/application',
+  SCHOLARSHIP = '/scholarship',
+  SETTING = '/setting',
+
+  // Auth
   SIGNIN = '/auth/signin',
   RESOLVE_TOKEN = '/auth/resolve-token',
-
-  SCHOLARSHIP = '/scholarship',
-  APPLICATION = '/application',
 }
--- src/constants/Path.ts
+++ src/constants/Path.ts
@@ -1,20 +1,22 @@
 export enum Path {
+  // Public
   HOME = '/',
-
-  PROFILE = '/profile',
   SCHOLARSHIP = '/scholarship',
 
+  // Student
+  PROFILE = '/profile',
   APPLY = '/apply',
   STATUS = '/status',
   HISTORY = '/history',
 
+  // Admin
   CONSIDER = '/consider',
   RECIPIENT = '/recipient',
   CREATE_SCHOLARSHIP = '/scholarship/create',
   EDIT_SCHOLARSHIP = '/scholarship/edit',
 
+  // Auth
   SIGNIN = '/signin',
   CALLBACK = '/cmuEntraIDCallback',
   SIGNOUT = '/signout',
-  TEST = '/test',
 }
