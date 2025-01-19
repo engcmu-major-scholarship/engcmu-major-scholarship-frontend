add640a2d18fc3432dd79fd47f47658ffdc3a47a
Yamami <yamami.fujita@gmail.com>
2025-01-20 05:42:43 +0700

    Refactor import statements for Loading component

--- src/pages/Signout/Signout.tsx
+++ src/pages/Signout/Signout.tsx
@@ -1,4 +1,4 @@
-import Loading from '../../components/Loading/Loading';
+import Loading from '../../components/Loading';
 import useSignoutController from './useSignoutController';
 
 const Signout = () => {
