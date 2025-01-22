5fa56185d9feacb46381ee4020caab3747ee1d09
Ezlostto <earth99101145@gmail.com>
2025-01-22 23:08:42 +0700

    Add teacher selection sample and identify image preview positions

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,9 @@
 const Profile = () => {
   return (
-    <div className="h-full w-full flex flex-col overflow-y-auto">
+    <div className="h-full w-full flex flex-col overflow-y-auto ">
       <div className="flex flex-col px-24 py-10 gap-6">
         <div className="inline-block">
-          <label className="font-bold text-gray-800 mr-4">ชื่อ-นามสกุล</label>
+          <label className="text-gray-800 mr-4 ">ชื่อ-นามสกุล</label>
           <input
             type="text"
             id="fname"
@@ -12,7 +12,7 @@ const Profile = () => {
           ></input>
         </div>
         <div className="inline-block">
-          <label className="font-bold text-gray-800 mr-4">รหัสนักศึกษา</label>
+          <label className="text-gray-800 mr-4">รหัสนักศึกษา</label>
           <input
             type="text"
             id="fname"
@@ -20,12 +20,17 @@ const Profile = () => {
             className="border border-gray-800 rounded px-3 py-1 hover:bg-gray-200"
           ></input>
         </div>
-        <div>
-          <label className="font-bold text-gray-800">อาจารย์ที่ปรึกษา</label>
+        <div className="inline-block">
+          <label className="text-gray-800 mr-4">อาจารย์ที่ปรึกษา</label>
+          <select className="border border-gray-800 rounded px-3 py-1 hover:bg-gray-200">
+            <option value="A">อาจารย์ A</option>
+            <option value="B">อาจารย์ B</option>
+            <option value="C">อาจารย์ C</option>
+          </select>
         </div>
 
         <div>
-          <label className="font-bold text-gray-800">รูปบัตรนักศึกษา</label>
+          <label className="text-gray-800">รูปบัตรนักศึกษา</label>
           <div className="px-3 py-1 mt-4">
             <label
               htmlFor="file-upload"
@@ -37,10 +42,11 @@ const Profile = () => {
             <button className="border border-gray-800 px-5 py-2 rounded hover:bg-gray-200 active:bg-gray-300">
               อัพโหลดรูป
             </button>
+            <h1 className="text-red-400">* Preview Image down here</h1>
           </div>
         </div>
         <div>
-          <label className="font-bold text-gray-800">รูปหน้าบัญชีธนาคาร</label>
+          <label className="text-gray-800">รูปหน้าบัญชีธนาคาร</label>
           <div className="px-3 py-1 mt-4">
             <label
               htmlFor="file-upload"
@@ -52,6 +58,7 @@ const Profile = () => {
             <button className="border border-gray-800 px-5 py-2 rounded hover:bg-gray-200 active:bg-gray-300">
               อัพโหลดรูป
             </button>
+            <h1 className="text-red-400">* Preview Image down here</h1>
           </div>
         </div>
         <div className="px-3 py-1 mt-4">
