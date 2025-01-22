5d230dfbb7a225d53fc66290a5eb55bcfd12b181
Yamami <yamami.fujita@gmail.com>
2025-01-22 22:08:19 +0700

    fix login flow so we don't have to do double login (#12)

--- src/providers/HttpClientProvider.tsx
+++ src/providers/HttpClientProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useCallback, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { ApiClient } from '../utils/Http/ApiClient';
 import { HttpClientContext } from '../contexts/HttpClientContext';
 import { useAuth } from '../hooks/useAuth';
@@ -19,8 +19,8 @@ const HttpClientProvider = ({ children }: { children: ReactNode }) => {
   const { token, setCMUAccount, setRoles } = useAuth();
   const [httpClient, setHttpClient] = useState<HttpClient>(new ApiClient());
 
-  const setHttpClientToken = useCallback(
-    (token: string) => {
+  useEffect(() => {
+    if (token !== null) {
       const httpClient = new ApiClient({ token });
       setHttpClient(httpClient);
       httpClient
@@ -32,15 +32,8 @@ const HttpClientProvider = ({ children }: { children: ReactNode }) => {
         .catch((err) => {
           console.error(err);
         });
-    },
-    [setCMUAccount, setRoles],
-  );
-
-  useEffect(() => {
-    if (token !== null && token !== '') {
-      setHttpClientToken(token);
     }
-  }, [setHttpClientToken, token]);
+  }, [setCMUAccount, setRoles, token]);
 
   return (
     <HttpClientContext.Provider value={httpClient}>
