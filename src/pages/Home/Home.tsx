f7a900c17ef167546a8f1457df91baceca7707f6
Yamami <yamami.fujita@gmail.com>
2025-02-13 23:32:48 +0700

    Refactor Home and Navbar components for improved layout and usability (#18)
    * Remove unused context and clean up Home component layout and reimplement navbar
    
    * Refactor Navbar controller by removing unused state and add icons to navigation items
    
    * Refactor Navbar component by simplifying state management and enhancing layout; add profile edit icon to roles access paths

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,41 +1,9 @@
-import { useContext } from 'react';
 import useHomeController from './useHomeController';
-import { RolesBaseAccessContext } from '../../contexts/RolesBaseAccessContext';
-import { useNavigate } from 'react-router';
-import { Path } from '../../constants/Path';
-import { createCMUAccountSignInUrl } from '../../utils/handleCMUAccountSignIn';
 
 const Home = () => {
   useHomeController();
-  const { accessibles } = useContext(RolesBaseAccessContext);
-  const navigate = useNavigate();
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center overflow-auto">
-      <div className="flex flex-row gap-8">
-        {accessibles.map((path, index) => {
-          if (
-            path.link == Path.HOME ||
-            path.link == createCMUAccountSignInUrl()
-          ) {
-            return null;
-          } else {
-            return (
-              <div className="flex flex-col items-center" key={index}>
-                <button
-                  className="flex justify-center items-center p-3 size-24 bg-[#b7cdcf] rounded-full border-8 border-[#dbe9ea]"
-                  onClick={() => {
-                    navigate(path.link);
-                  }}
-                >
-                  {path.icon}
-                </button>
-                <span>{path.label}</span>
-              </div>
-            );
-          }
-        })}
-      </div>
-    </div>
+    <div className="h-full w-full flex flex-col justify-center items-center overflow-auto"></div>
   );
 };
 
