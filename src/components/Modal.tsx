a38edcf1012f5d2b03c9e41db9fa0e1517becc35
Yamami <yamami.fujita@gmail.com>
2025-02-14 21:46:03 +0700

    fix modal opacity issue

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const Modal = ({ children }: { children?: ReactNode }) => {
   return (
-    <div className="absolute inset-0 max-h-screen max-w-screen overflow-auto bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="absolute inset-0 max-h-screen max-w-screen overflow-auto bg-black/50 flex items-center justify-center">
       {children}
     </div>
   );
